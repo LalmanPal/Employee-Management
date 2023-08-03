74a687b50a779957f36ff489216d929af7da2f8e
Lalman <lalmanpal004@gmail.com>
2023-08-03 18:03:24 +0530

    This is my second commit

--- SpringBootWebApp/src/main/java/com/crud/service/EmployeeServiceImpl.java
+++ SpringBootWebApp/src/main/java/com/crud/service/EmployeeServiceImpl.java
@@ -1,8 +1,13 @@
 package com.crud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.crud.entity.Employee;
@@ -18,5 +23,47 @@ public class EmployeeServiceImpl implements EmployeeService {
 		
 		return repo.findAll();
 	}
+
+	@Override
+	public void saveEmployee(Employee employee) {
+		repo.save(employee);
+		
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> opt = repo.findById(id);
+		Employee emp=null;
+		if(opt.isPresent()) {
+			return emp=opt.get();
+		}
+		else {
+			throw new RuntimeException("User with id ="+id+" is not present in record!");
+		}	
+		
+	}
+
+	@Override
+	public void deleteEmployeeById(long id) {
+		this.repo.deleteById(id);
+		
+	}
+
+//	@Override
+//	public Page<Employee> findPaginated(int pageNo, int pageSize) {
+//		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+//		 return this.repo.findAll(pageable);
+//	}
+
+	@Override
+	public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+		 Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+	            Sort.by(sortField).descending();
+
+	        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+	        return this.repo.findAll(pageable);
+	}
+	
+	
 
 }
